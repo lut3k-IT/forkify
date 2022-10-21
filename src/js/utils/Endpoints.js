b098972e2fd75167efc45e9dc8ef01631518df37
Kacper Lutynski <lut3k777@gmail.com>
2022-10-22 00:06:57 +0200

    parcel error

---
+++ src/js/utils/Endpoints.js
@@ -0,0 +1,3 @@
+export default class Endpoints {
+  static RECIPES = 'recipes';
+}
---
+++ src/js/utils/config.js
@@ -0,0 +1,11 @@
+// api key
+export const API_KEY = 'b3806cbe-6bbb-46f3-ba61-8c39c10d36e7';
+
+// paths
+export const BASE_URL = 'https://forkify-api.herokuapp.com/api/v2';
+
+// values
+export const ABORT_TIMEOUT = 10;
+export const SERACH_RESULTS_PER_PAGE = 10;
+export const SEARCH_INIT_PAGE = 1;
+export const MODAL_CLOSE_SEC = 2.5;
